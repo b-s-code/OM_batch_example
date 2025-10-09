0e63c06d5eb9df3824d7717eecbe922739ffac94
b-s-code <89502375+b-s-code@users.noreply.github.com>
2025-10-09 11:55:37 +0800

    Use relative path (only for convenience right now).

--- master.sh
+++ master.sh
@@ -5,7 +5,7 @@ NUM_SIMULATIONS_PER_NODE=128
 
 # Seems like this will create many files.
 # TODO: check.
-split -l ${NUM_SIMULATIONS_PER_NODE} -d ${MYSCRATCH}/input_params.txt batch_
+split -l ${NUM_SIMULATIONS_PER_NODE} -d ./input_params.txt batch_
 
 for batch in batch_*; do
     sbatch --export=PARAMS_LIST=$batch om_batch.sh
