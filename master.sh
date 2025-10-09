74cb8f5dcf9efe76e4faf88c8442c5f7fd162c63
b-s-code <89502375+b-s-code@users.noreply.github.com>
2025-10-09 11:40:46 +0800

    Reduce num chunks per node to 1.

--- master.sh
+++ master.sh
@@ -1,6 +1,11 @@
 #!/bin/bash -l
 
-split -l 384 -d ${MYSCRATCH}/input_params.txt batch_
+# Can be >128.
+NUM_SIMULATIONS_PER_NODE=128
+
+# Seems like this will create many files.
+# TODO: check.
+split -l ${NUM_SIMULATIONS_PER_NODE} -d ${MYSCRATCH}/input_params.txt batch_
 
 for batch in batch_*; do
     sbatch --export=PARAMS_LIST=$batch om_batch.sh
